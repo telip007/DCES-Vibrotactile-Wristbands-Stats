f91bb1c6eb084ccb8a575f235a3cc2baa6f9f73f
Talip Göksu <talip@pace.car>
2021-09-05 21:07:46 +0200

    Add hypotheses 1-7

--- get_data.R
+++ get_data.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(readxl)
 
 participant_data <- dir("./data", full.names = T) %>% map_df(
   ~read_csv(., col_types = cols_only(
@@ -12,4 +13,59 @@ participant_data <- dir("./data", full.names = T) %>% map_df(
     wristRotation = col_double()
   )
   )
+)
+
+# participantIDs for walking
+list <- c(11, 21, 31, 41, 52, 62, 72, 82, 91, 101, 111, 121, 132, 142, 152, 162, 171, 181, 192, 202)
+walking_first <- c(1, 2, 3, 4, 9, 10, 11, 12, 17, 18)
+jigsaw_first <- c(5, 6, 7, 8, 13, 14, 15, 16, 19, 20)
+
+participant_data <- participant_data %>% mutate(task = case_when(
+  participantId %in% list ~ 0, # gehen
+  TRUE ~ 1 # puzzeln
+))
+
+participant_data <- participant_data %>% mutate(expectedAngle = (expectedAngle + 180) %% 360)
+participant_data <- participant_data %>% mutate(drawnAngle = (drawnAngle + 180) %% 360)
+participant_data <- participant_data %>% mutate(angleDelta = abs(((abs(expectedAngle - drawnAngle) + 180) %% 360) - 180))
+participant_data <- participant_data %>% mutate(trialDuration = trialEnd - trialStart)
+
+mental_data_walking <- read_excel("./Frageboegen/Gehen.xlsx")
+mental_data_jigsaw <- read_excel("./Frageboegen/Puzzlen.xlsx")
+
+mental_data_walking <- mental_data_walking %>% mutate(firstTask = case_when(
+  participantId %in% walking_first ~ TRUE,
+  TRUE ~ FALSE
+))
+
+mental_data_jigsaw <- mental_data_jigsaw %>% mutate(firstTask = case_when(
+  participantId %in% jigsaw_first ~ TRUE,
+  TRUE ~ FALSE
+))
+
+participant_ids <- c()
+first_task_demand <- c()
+second_task_demand <- c()
+
+for (i in 1:20) {
+  mental_walking <- mental_data_walking[i,]
+  mental_jigsaw <- mental_data_jigsaw[i,]
+
+  participant_ids <- append(participant_ids, i)
+  if (mental_walking$firstTask) {
+    first_task_demand <- append(first_task_demand, mental_walking$`Mental Demand`)
+    second_task_demand <- append(second_task_demand, mental_jigsaw$`Mental Demand`)
+  } else {
+    second_task_demand <- append(second_task_demand, mental_walking$`Mental Demand`)
+    first_task_demand <- append(first_task_demand, mental_jigsaw$`Mental Demand`)
+  }
+}
+
+diffs <- first_task_demand - second_task_demand
+
+mental_demand_data <- data.frame(
+  participantId = participant_ids,
+  firstTask = first_task_demand,
+  secondTask = second_task_demand,
+  difference = diffs
 )
--- main.R
+++ main.R
@@ -1,7 +1,40 @@
 source("get_data.R")
 
-# Do something with data
-# This just an example file
-# We should have a separate script for every analysis.
-view(participant_data)
+# Hypothese 1
+describeBy(participant_data$angleDelta, participant_data$condition)
+wilcox.test(participant_data$angleDelta, participant_data$condition, 
+            paired = TRUE, exact = FALSE, correct = FALSE, conf.int = TRUE, 
+            alternative = "two.sided")
+
+# Hypothese 2
+describeBy(participant_data$trialDuration, participant_data$condition)
+wilcox.test(participant_data$trialDuration, participant_data$condition, 
+            paired = TRUE, exact = FALSE, correct = FALSE, conf.in = TRUE, 
+            alternative =  "two.sided")
+
+# Hypothese 3
+describeBy(participant_data$trialDuration, participant_data$task)
+wilcox.test(participant_data$trialDuration, participant_data$task,
+            paired = TRUE, exact = FALSE, correct = FALSE, conf.in = TRUE,
+            alternative = "two.sided")
+
+# Hypothese 4
+describeBy(participant_data$angleDelta, participant_data$task)
+wilcox.test(participant_data$angleDelta, participant_data$task, 
+            paired = TRUE, exact = FALSE, correct = FALSE, conf.int = TRUE, 
+            alternative = "two.sided")
+
+# Hypothese 5
+angleDelta_reg <- lm(participant_data$angleDelta~participant_data$trialNumber)
+plot(participant_data$trialNumber,participant_data$angleDelta,col="blue", ylim=c(30,40))
+abline(angleDelta_reg, col="red")
+summary(angleDelta_reg)
+
+# Hypothese 6
+reactionTime_reg <- lm(participant_data$trialDuration~participant_data$trialNumber)
+plot(participant_data$trialNumber,participant_data$trialDuration,col="blue", ylim=c(200,4000))
+abline(reactionTime_reg, col="red")
+summary(reactionTime_reg)
+
+# Hypothese 7
 
